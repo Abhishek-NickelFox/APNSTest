1fcbf4161088fc5e665140e64758a77054e65939
Abhishek Thapliyal <abhishek@nickelfox.com>
2017-06-28 14:24:20 +0530

    #1

---
+++ APNSTest/APNSServiceExt/NotificationService.swift
@@ -0,0 +1,82 @@
+//
+//  NotificationService.swift
+//  APNSServiceExt
+//
+//  Created by Abhishek on 28/06/17.
+//  Copyright © 2017 Abhishek. All rights reserved.
+//
+
+import UserNotifications
+
+class NotificationService: UNNotificationServiceExtension {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        func failEarly() {
+            contentHandler(request.content)
+        }
+        
+        guard let content = (request.content.mutableCopy() as? UNMutableNotificationContent) else {
+            return failEarly()
+        }
+        
+        guard let attachmentURL = content.userInfo["attachment-url"] as? String else {
+            return failEarly()
+        }
+        
+        
+        let imageData = NSData(contentsOf: URL(string: attachmentURL)!)!
+        
+        
+//        guard let imageData = Data(contentsOf: URL(string: attachmentURL)!) else { return failEarly() }
+        guard let attachment = UNNotificationAttachment.create(imageFileIdentifier: "image.gif", data: imageData, options: nil) else { return failEarly() }
+        
+        content.attachments = [attachment]
+        contentHandler(content.copy() as! UNNotificationContent)
+        
+        if let bestAttemptContent = bestAttemptContent {
+            // Modify the notification content here...
+            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
+            
+            contentHandler(bestAttemptContent)
+        }
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+}
+
+extension UNNotificationAttachment {
+    
+    /// Save the image to disk
+    static func create(imageFileIdentifier: String, data: NSData, options: [NSObject : AnyObject]?) -> UNNotificationAttachment? {
+    let fileManager = FileManager.default
+    let tmpSubFolderName = ProcessInfo.processInfo.globallyUniqueString
+    let tmpSubFolderURL = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(tmpSubFolderName, isDirectory: true)
+    
+    do {
+    try fileManager.createDirectory(at: tmpSubFolderURL!, withIntermediateDirectories: true, attributes: nil)
+//    let fileURL = tmpSubFolderURL?.URLByAppendingPathComponent(imageFileIdentifier)
+        let fileURL = tmpSubFolderURL?.appendingPathComponent(imageFileIdentifier)
+        
+    try data.write(to: fileURL!, options: [])
+    let imageAttachment = try UNNotificationAttachment(identifier: imageFileIdentifier, url: fileURL!, options: options)
+    return imageAttachment
+    } catch let error {
+    print("error \(error)")
+    }
+    
+    return nil
+    }
+}
